b3490dd27cdeb5c012af2e99ce7198a8592138d2
Sohaib-Azhar010 <sohaibazhar04@gmail.com>
2025-04-13 18:20:04 +0500

    frontend pages added

--- users/views.py
+++ users/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home.html')
+
+def cart_view(request):
+    return render(request, 'cart.html')
+
+def checkout_view(request):
+    return render(request, 'checkout.html')
+
+def product_view(request):
+    return render(request, 'product.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def allproducts_view(request):
+    return render(request, 'all_products.html')
+
+def blogs_view(request):
+    return render(request, 'blog.html')
+
+def thanks_view(request):
+    return render(request, 'thanks.html')
