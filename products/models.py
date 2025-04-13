63ed316f245d8bf645a6e62217df0357248296f3
abdullah956 <abdullaharshed956@gmail.com>
2025-04-13 19:56:01 +0500

    category product model base

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from config.models import BasedModel
 
-# Create your models here.
+
+class Category(BasedModel):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+ 
+class Product(BasedModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/')
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
