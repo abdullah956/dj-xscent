bf09c5065eeeb81bef6c55d6dfe0a2927f965843
abdullah956 <abdullaharshed956@gmail.com>
2025-04-13 19:30:13 +0500

    blog model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from config.models import BasedModel
 
-# Create your models here.
+
+class Blog(BasedModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog_images/')
+
+    def __str__(self):
+        return self.title
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Blog
+
 
 def home_view(request):
     return render(request, 'home.html')
 
 def blogs_view(request):
-    return render(request, 'users/blog.html')
+    blogs = Blog.objects.order_by('created_at')
+    return render(request, 'users/blog.html', {'blogs': blogs})
 
 def contact_view(request):
     return render(request, 'users/contact.html')
