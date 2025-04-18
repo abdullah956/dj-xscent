95e87912b420b77f9880dbbcce5032e484d9f184
abdullah956 <abdullaharshed956@gmail.com>
2025-04-18 15:46:34 +0500

    all prod by cat filter

--- products/views.py
+++ products/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from .models import Product, Category
+
 
 def product_view(request):
     return render(request, 'products/product.html')
 
+
 def allproducts_view(request):
-    return render(request, 'products/all_products.html')
+    category_id = request.GET.get('category')
+    price_filter = request.GET.get('price')
+    products = Product.objects.all()
+
+    if category_id:
+        products = products.filter(category_id=category_id)
+
+    if price_filter == 'below1500':
+        products = products.filter(price__lt=1500)
+    elif price_filter == '1500to2500':
+        products = products.filter(price__gte=1500, price__lte=2500)
+    elif price_filter == 'above2500':
+        products = products.filter(price__gt=2500)
+
+    categories = Category.objects.all()
+    return render(request, 'products/all_products.html', {
+        'products': products,
+        'categories': categories,
+    })
