37d8372029c0ceff4b8e6814c1198ab13a998843
abdullah956 <abdullaharshed956@gmail.com>
2025-04-18 16:37:39 +0500

    order model

--- checkouts/models.py
+++ checkouts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from config.models import BasedModel
 
-# Create your models here.
+class Orders(BasedModel):
+    PAYMENT_METHOD_CHOICES = [
+        ('cod', 'Cash on Delivery'),
+        ('online', 'Online '),
+    ]
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    street_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PAYMENT_METHOD_CHOICES,
+        default='cod',
+    )
+    cart_data = models.JSONField(default=dict)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.payment_method}'
--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'users',
     'products',
     'carts',
+    'checkouts',
 ]
 
 MIDDLEWARE = [
