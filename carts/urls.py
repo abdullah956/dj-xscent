a0b715c8d5906146cc59b105538c2bdce544ec5d
abdullah956 <abdullaharshed956@gmail.com>
2025-04-18 16:03:35 +0500

    add remove cart

--- carts/urls.py
+++ carts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import cart_view,checkout_view,thanks_view
+from .views import cart_view,checkout_view,thanks_view,add_to_cart,remove_from_cart
 
 urlpatterns = [
     path('cart/', cart_view, name='cart'),
+    path('add-to-cart/<int:id>/', add_to_cart, name='add_to_cart'),
     path('checkout/', checkout_view, name='checkout'),
     path('thanks/', thanks_view, name='thanks'),
+    path('remove-from-cart/<int:id>/', remove_from_cart, name='remove_from_cart'),
 
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from products.models import Product
+from django.shortcuts import get_object_or_404, redirect
+
 
 def cart_view(request):
     return render(request, 'carts/cart.html')
@@ -8,3 +11,30 @@ def checkout_view(request):
 
 def thanks_view(request):
     return render(request, 'carts/thanks.html')
+
+
+def add_to_cart(request, id):
+    product = get_object_or_404(Product, id=id)
+    cart = request.session.get('cart', {})
+
+    if str(id) in cart:
+        cart[str(id)]['quantity'] += 1
+    else:
+        cart[str(id)] = {
+            'name': product.name,
+            'price': float(product.price),
+            'image': product.image.url,
+            'quantity': 1
+        }
+
+    request.session['cart'] = cart
+    return redirect('cart')
+
+def remove_from_cart(request, id):
+    cart = request.session.get('cart', {})
+
+    if str(id) in cart:
+        del cart[str(id)]
+        request.session['cart'] = cart
+
+    return redirect('cart')
